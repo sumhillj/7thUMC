738a15faba1057f9f2c52229f9d5d1f27e707070
sumhillj <sumhillj@gmail.com>
2024-10-03 20:41:49 +0900

    2-2 mission

---
+++ Chapter2/2-2/src/App.jsx
@@ -0,0 +1,14 @@
+import Movies from '../mocks/Movies'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div style={{ width: "100%", padding: "20px", boxSizing: "border-box" }}>
+        <Movies />
+      </div>
+    </>
+  )
+}
+
+export default App
